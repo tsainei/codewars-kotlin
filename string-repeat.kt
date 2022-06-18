fc0032783ef1d7703ee5affceb003899938d776c
tsainei yuan <hi@tsainei.com>
2022-06-18 22:14:47 +0200

    third challenge

---
+++ string-repeat.kt
@@ -0,0 +1,3 @@
+/* Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times. */
+
+fun repeatStr(r: Int, str: String) : String = str.repeat(r)
