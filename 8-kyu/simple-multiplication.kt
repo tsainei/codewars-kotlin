401f5ee88d739a83c695993793ca99bdfccec71f
tsainei yuan <hi@tsainei.com>
2022-06-21 13:26:10 +0200

    sixth challenge

--- 8-kyu/simple-multiplication.kt
+++ 8-kyu/simple-multiplication.kt
@@ -1,2 +1,13 @@
 /* This kata is about multiplying a given number by eight if it is an even number and by nine otherwise./*  */
- */
+
+fun simpleMultiplication(n: Int): Int {
+  if(n % 2 == 0) {
+      return n * 8
+  } else {
+      return n * 9
+  }
+}
+
+// fun simpleMultiplication(n: Int): Int {
+//   return n * (8 + n%2);
+// }
